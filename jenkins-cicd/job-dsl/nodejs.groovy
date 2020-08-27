caccbc10d3de9eeddfbad7dd2b426cec64a0046e
VijayRaghuvanshi <rg.vijay.k@gmail.com>
2020-08-27 18:17:48 +0530

    updated file

--- jenkins-cicd/job-dsl/nodejs.groovy
+++ jenkins-cicd/job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/VijayRaghuvanshi/docker-example.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('rg.vijay.k@gmail.com')
         }
     }
     triggers {
